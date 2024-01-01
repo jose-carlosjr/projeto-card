187dbe4dcb5b51cbc22c5218dd5625a7c3ab9f09
Jose <josecarlos099916@gmail.com>
2023-12-31 21:58:35 -0300

    Concluí a nav

--- projeto-card/src/components/Nav.jsx
+++ projeto-card/src/components/Nav.jsx
@@ -2,39 +2,44 @@ import { FaWhatsapp } from "react-icons/fa";
 import { CiFacebook } from "react-icons/ci";
 import { FaInstagram } from "react-icons/fa";
 import { FiMapPin } from "react-icons/fi";
+import Logo from '../assets/logo.png';
 
 function Nav() {
     return(
         <>
             <nav>
-                <div className="navtop">
-                    <div className="navtop-social-media">
-                        <a href="#">
-                            <span><CiFacebook />Facebook</span>
-                        </a>
-                        <a href="#">
-                            <span><FaInstagram />Instagram</span>
-                        </a>
-                    </div>
-                    <div className="navtop-endereco">
-                        <span><FaWhatsapp />(21) 3439-103</span>
-                        <span><FaWhatsapp />(21) 99174-1946</span>
-                        <a href="#"><FiMapPin />Endereço</a>
+                <div className="navtop-container">
+                    <div className="navtop container">
+                        <div className="navtop-social-media">
+                            <a href="#">
+                                <span><CiFacebook />Facebook</span>
+                            </a>
+                            <a href="#">
+                                <span><FaInstagram />Instagram</span>
+                            </a>
+                        </div>
+                        <div className="navtop-endereco">
+                            <span><FaWhatsapp />(21) 3439-103</span>
+                            <span><FaWhatsapp />(21) 99174-1946</span>
+                            <a href="#"><FiMapPin />Endereço</a>
+                        </div>
                     </div>
                 </div>
 
-                <div className="navbottom">
-                    <div className="logo">
-                        <img src="" alt="" />
-                    </div>
-                    <div className="menu">
-                        <ul>
-                            <li><a href="#"></a></li>
-                            <li><a href="#"></a></li>
-                            <li><a href="#"></a></li>
-                            <li><a href="#"></a></li>
-                            <li><a href="#"></a></li>
-                        </ul>
+                <div className="navbottom-container">
+                    <div className="navbottom container">
+                        <div className="logo">
+                            <img src={Logo} alt="Logo do site" />
+                        </div>
+                        <div className="menu">
+                            <ul>
+                                <li><a href="#">Home</a></li>
+                                <li><a href="#">Tratamentos</a></li>
+                                <li><a href="#">Sobre</a></li>
+                                <li><a href="#">Contato</a></li>
+                                <li><a href="#">Blog</a></li>
+                            </ul>
+                        </div>
                     </div>
                 </div>
             </nav>
