53cba66f40fa3bfecd0f0ceca3cc111c6c197cea
Jose <josecarlos099916@gmail.com>
2023-12-31 21:12:58 -0300

    criando a nav

--- projeto-card/src/components/Nav.jsx
+++ projeto-card/src/components/Nav.jsx
@@ -1,8 +1,42 @@
+import { FaWhatsapp } from "react-icons/fa";
+import { CiFacebook } from "react-icons/ci";
+import { FaInstagram } from "react-icons/fa";
+import { FiMapPin } from "react-icons/fi";
+
 function Nav() {
     return(
         <>
             <nav>
-                <span>Projeto Card</span>
+                <div className="navtop">
+                    <div className="navtop-social-media">
+                        <a href="#">
+                            <span><CiFacebook />Facebook</span>
+                        </a>
+                        <a href="#">
+                            <span><FaInstagram />Instagram</span>
+                        </a>
+                    </div>
+                    <div className="navtop-endereco">
+                        <span><FaWhatsapp />(21) 3439-103</span>
+                        <span><FaWhatsapp />(21) 99174-1946</span>
+                        <a href="#"><FiMapPin />Endereço</a>
+                    </div>
+                </div>
+
+                <div className="navbottom">
+                    <div className="logo">
+                        <img src="" alt="" />
+                    </div>
+                    <div className="menu">
+                        <ul>
+                            <li><a href="#"></a></li>
+                            <li><a href="#"></a></li>
+                            <li><a href="#"></a></li>
+                            <li><a href="#"></a></li>
+                            <li><a href="#"></a></li>
+                        </ul>
+                    </div>
+                </div>
             </nav>
         </>
     )
